e83a3c4d790506b5a060564194edd356cee7f762
yjj <2112106066@e.gzhu.edu.cn>
2025-06-18 10:25:23 +0800

    update actions

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include <cstdio>
-#include <print>
+#include <iostream>
+#include <format>
 
 int main()
 {
     for (int i = 0; i < 10; i++)
-        std::print("{}\n", i);
+        std::cout << std::format("{}\n", i);
     return EXIT_SUCCESS;
 }
